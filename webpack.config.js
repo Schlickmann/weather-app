3b09b85f23c7e7acb129bdf7123cb93a0d07fcd0
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-03-14 08:35:13 -0700

    added favicon and a couple of svg

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src', 'index.js'),
@@ -9,6 +10,11 @@ module.exports = {
   devServer: {
     contentBase: path.resolve(__dirname, 'public')
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      favicon: "./src/assets/moon.svg"
+    })
+  ],
   module: {
     rules: [
       { 
@@ -19,6 +25,10 @@ module.exports = {
       {
         test: /\.css$/,
         use: [ { loader: 'style-loader' }, { loader: 'css-loader' } ]
+      },
+      {
+        test: /.*\.(gif|png|jpe?g)$/i,
+        use: { loader: 'file-loader' }
       }
     ]
   }
