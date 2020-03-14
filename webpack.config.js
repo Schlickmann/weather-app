bab9dde245bcfc64b0d510fe48bb5adfafee1337
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-03-14 12:46:52 -0700

    changed webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,10 @@
 const path = require('path');
 const fs = require('fs');
 const webpack = require('webpack');
-const dotenv = require('dotenv');
+const Dotenv = require('dotenv-webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = (env) => {
-  // Get the root path (assuming your webpack config is in the root of your project!)
-  const currentPath = path.join(__dirname);
-  
-  // Create the fallback path (the production .env)
-  const basePath = currentPath + '/.env';
-
-  // We're concatenating the environment name to our filename to specify the correct env file!
-  const envPath = basePath + '.' + env.ENVIRONMENT;
-
-  // Check if the file exists, otherwise fall back to the production .env
-  const finalPath = fs.existsSync(envPath) ? envPath : basePath;
-
-  // Set the path parameter in the dotenv config
-  const fileEnv = dotenv.config({ path: finalPath }).parsed;
-  
-  // reduce it to a nice object, the same as before (but with the variables from the file)
-  const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
-    return prev;
-  }, {});
-
-  console.log('Env variables ',envKeys)
 
   return {
     entry: path.resolve(__dirname, 'src', 'index.js'),
@@ -40,7 +18,7 @@ module.exports = (env) => {
         template: path.resolve(__dirname, 'public', 'index.html'),
         favicon: path.resolve(__dirname, 'src', 'assets', 'logo.svg')
       }),
-       new webpack.DefinePlugin(envKeys)
+      new Dotenv()
     ],
     devServer: {
       contentBase: path.resolve(__dirname, 'public')
